d152dfbcc426a03e70ab6fb32d4843b50e7b9c86
Jason L. Walker <jasonlanewalker@gmail.com>
2023-02-19 01:27:07 -0600

    Changed editors to work directly with object in state

--- src/components/todo-item/TodoItemEditor.tsx
+++ src/components/todo-item/TodoItemEditor.tsx
@@ -21,43 +21,87 @@ import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import moment from "moment";
 import { useState } from "react";
+import { TodoTag } from "../todo-tag/TodoTagView";
+import { Guid, TodoItem } from "./TodoItemView";
 
 interface TodoItemEditorProps {
-  tags: string[];
+  tags: TodoTag[];
+  addTodoItem: (item: TodoItem) => void;
 }
 
-export function TodoItemEditor({ tags }: TodoItemEditorProps) {
-  const [dueDate, setDueDate] = useState<moment.Moment | null>(moment());
+const initialItem: TodoItem = {
+  description: "",
+  isActive: true,
+  isComplete: false,
+  rollsOver: false,
+  dueDate: moment(),
+  tagIds: [],
+};
+
+export function TodoItemEditor({ tags, addTodoItem }: TodoItemEditorProps) {
+  const [todoItem, setTodoItem] = useState<TodoItem>(initialItem);
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
-  
+
   const handleDueDateChange = (newDate: moment.Moment | null) => {
-    setDueDate(newDate);
-  }
+    if (newDate !== null) {
+      setTodoItem({ ...todoItem, dueDate: newDate });
+    }
+  };
 
   const handleTagsChanged = (event: SelectChangeEvent<typeof selectedTags>) => {
-    const {target: { value}, } = event;
-    setSelectedTags(
-      typeof value === "string" ? value.split(',') : value,
-    )
-  }
-  
+    const {
+      target: { value },
+    } = event;
+    setSelectedTags(typeof value === "string" ? value.split(",") : value);
+    setTodoItem({ ...todoItem, tagIds: getSelectedTagIds() });
+  };
+
+  const getSelectedTagIds = (): Guid[] => {
+    return tags
+      .filter((t) => selectedTags.includes(t.name) !== undefined)
+      .map((x) => x.id);
+  };
+
   return (
     <>
       <Paper sx={{ maxWidth: 800 }}>
         <Typography variant="h5">Create Todo Item</Typography>
         <Stack direction="column">
-          <TextField variant="standard" />
+          <TextField
+            variant="standard"
+            value={todoItem.description}
+            onChange={(e) =>
+              setTodoItem({ ...todoItem, description: e.target.value })
+            }
+          />
           <FormGroup>
             <FormControlLabel
-              control={<Checkbox defaultChecked />}
+              control={
+                <Checkbox
+                  checked={todoItem.isActive}
+                  onChange={(e) =>
+                    setTodoItem({ ...todoItem, isActive: e.target.checked })
+                  }
+                />
+              }
               label="Active"
             />
-            <FormControlLabel control={<Checkbox />} label="Rolling item" />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={todoItem.rollsOver}
+                  onChange={(e) =>
+                    setTodoItem({ ...todoItem, rollsOver: e.target.checked })
+                  }
+                />
+              }
+              label="Rolling item"
+            />
           </FormGroup>
           <LocalizationProvider dateAdapter={AdapterMoment}>
             <DatePicker
               label="Due Date"
-              value={dueDate}
+              value={todoItem.dueDate}
               onChange={handleDueDateChange}
               minDate={moment()}
               renderInput={(params) => <TextField {...params} />}
@@ -81,17 +125,16 @@ export function TodoItemEditor({ tags }: TodoItemEditorProps) {
               )}
             >
               {tags.map((tag) => (
-                <MenuItem
-                  key={tag}
-                  value={tag}
-                >
-                  {tag}
+                <MenuItem key={tag.id} value={tag.name}>
+                  {tag.name}
                 </MenuItem>
               ))}
             </Select>
           </FormControl>
-        <Button variant="contained">Save Todo</Button>
-        </Stack>        
+          <Button variant="contained" onClick={() => addTodoItem(todoItem)}>
+            Save Todo
+          </Button>
+        </Stack>
       </Paper>
     </>
   );
--- src/components/todo-tag/TodoTagEditor.tsx
+++ src/components/todo-tag/TodoTagEditor.tsx
@@ -1,24 +1,30 @@
 import { Button, Stack, TextField, Typography } from "@mui/material";
 import { useState } from "react";
+import { TodoTag } from "./TodoTagView";
 
 interface TodoTagEditorProps {
-  addTag: (event: string) => void
+  addTag: (tag: TodoTag) => void;
 }
 
+const initialItem: TodoTag = {
+  id: "",
+  name: "",
+};
+
 export default function TodoTagEditor({ addTag }: TodoTagEditorProps) {
-  const [tag, setTag] = useState<string>("");
+  const [tag, setTag] = useState<TodoTag>(initialItem);
 
   return (
     <>
       <Stack direction="row">
         <Typography variant="h5">Create Tag</Typography>
-        <TextField variant="standard" 
-          onChange={e => setTag(e.target.value)}
+        <TextField
+          variant="standard"
+          onChange={(e) => {
+            setTag({ id: e.target.value + "-" + Math.random(), name: e.target.value });
+          }}
         />
-        <Button 
-          variant="contained"
-          onClick={() => addTag(tag)}
-        >
+        <Button variant="contained" onClick={() => addTag(tag)}>
           Save Tag
         </Button>
       </Stack>
