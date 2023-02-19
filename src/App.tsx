20ad360d9b692507aa571bec2a3d69c81e6d8f18
Jason L. Walker <jasonlanewalker@gmail.com>
2023-02-19 01:27:22 -0600

    Fixed props passed to components

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import { NavigationBar } from "./components/common/navigation/NavigationBar";
 import React, { useState } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import TodoTagEditor from "./components/todo-tag/TodoTagEditor";
-import { TodoTagView } from "./components/todo-tag/TodoTagView";
+import { TodoTag, TodoTagView } from "./components/todo-tag/TodoTagView";
 import { TodoItemEditor } from "./components/todo-item/TodoItemEditor";
+import { TodoItem, TodoItemView } from "./components/todo-item/TodoItemView";
 
 function App() {
-  const [tags, setTags] = useState<string[]>([]);
-  const addTag = (tag: string) => {
+  const [tags, setTags] = useState<TodoTag[]>([]);
+  const [todoItems, setTodoItems] = useState<TodoItem[]>([]);
+  
+  const addTag = (tag: TodoTag) => {
     setTags([tag, ...tags]);
+  }
+
+  const addTodoItem = (item: TodoItem) => {
+    setTodoItems([...todoItems, item]);
   }
 
   return (
@@ -19,7 +26,8 @@ function App() {
         <NavigationBar />
         <TodoTagEditor addTag={addTag} />
         <TodoTagView tags={tags}/>
-        <TodoItemEditor tags={tags} />
+        <TodoItemEditor tags={tags} addTodoItem={addTodoItem} />
+        <TodoItemView todoItems={todoItems} />
       </React.Fragment>
     </div>
   );
