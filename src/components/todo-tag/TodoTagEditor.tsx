a96e14fd2ff9bf87163d3ec5917c969c139c5bc7
Jason L. Walker <jasonlanewalker@gmail.com>
2023-02-19 01:35:52 -0600

    Remove unnecessary empty tag

--- src/components/todo-tag/TodoTagEditor.tsx
+++ src/components/todo-tag/TodoTagEditor.tsx
@@ -15,19 +15,20 @@ export default function TodoTagEditor({ addTag }: TodoTagEditorProps) {
   const [tag, setTag] = useState<TodoTag>(initialItem);
 
   return (
-    <>
-      <Stack direction="row">
-        <Typography variant="h5">Create Tag</Typography>
-        <TextField
-          variant="standard"
-          onChange={(e) => {
-            setTag({ id: e.target.value + "-" + Math.random(), name: e.target.value });
-          }}
-        />
-        <Button variant="contained" onClick={() => addTag(tag)}>
-          Save Tag
-        </Button>
-      </Stack>
-    </>
+    <Stack direction="row">
+      <Typography variant="h5">Create Tag</Typography>
+      <TextField
+        variant="standard"
+        onChange={(e) => {
+          setTag({
+            id: e.target.value + "-" + Math.random(),
+            name: e.target.value,
+          });
+        }}
+      />
+      <Button variant="contained" onClick={() => addTag(tag)}>
+        Save Tag
+      </Button>
+    </Stack>
   );
 }
