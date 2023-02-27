c240e6895b493071684f45a623b037d36f4dd755
Jason L. Walker <jasonlanewalker@gmail.com>
2023-02-26 23:54:52 -0600

    Refactored tag API operations to use contexts/reducers

--- src/components/todo-tag/TodoTagEditor.tsx
+++ src/components/todo-tag/TodoTagEditor.tsx
@@ -1,18 +1,27 @@
 import { Button, Stack, TextField, Typography } from "@mui/material";
 import { useState } from "react";
-import { ITodoTagResponse } from "../../api/api-client";
+import { ITodoTagResponse, TodoTagRequest } from "../../api/api-client";
+import { useTodoTagsPOSTMutation } from "../../api/api-client/Query";
+import { useTagsDispatch } from "../../contexts/TagsContext";
 
-interface TodoTagEditorProps {
-  addTag: (tag: ITodoTagResponse) => void;
-}
 
 const initialItem: ITodoTagResponse = {
   id: "",
   name: "",
 };
 
-export default function TodoTagEditor({ addTag }: TodoTagEditorProps) {
+export default function TodoTagEditor() {
   const [tag, setTag] = useState<ITodoTagResponse>(initialItem);
+
+  const dispatch = useTagsDispatch();
+
+  const saveTag = useTodoTagsPOSTMutation({
+    onSuccess: () => dispatch({type: "require-refetch"})
+  });
+
+  const handleClick = () => {
+    saveTag.mutate(new TodoTagRequest({ ...tag }))
+  };
 
   return (
     <Stack direction="row">
@@ -25,7 +34,7 @@ export default function TodoTagEditor({ addTag }: TodoTagEditorProps) {
           });
         }}
       />
-      <Button variant="contained" onClick={() => addTag(tag)}>
+      <Button variant="contained" onClick={handleClick}>
         Save Tag
       </Button>
     </Stack>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,16 +1,10 @@
 import { useEffect, useState } from "react";
 import {
-  CreateTodoItemRequest,
-  ICreateTodoItemRequest,
   ITodoItemResponse,
-  ITodoTagResponse,
-  TodoTagRequest,
 } from "../api/api-client";
 import {
   useTodoItemsAllQuery,
-  useTodoItemsMutation,
   useTodoTagsAllQuery,
-  useTodoTagsPOSTMutation,
 } from "../api/api-client/Query";
 import { TodoItemEditor } from "../components/todo-item/TodoItemEditor";
 import { TodoItemListView } from "../components/todo-item/TodoItemListView";
@@ -19,23 +13,9 @@ import { TodoTagListView } from "../components/todo-tag/TodoTagListView";
 
 export function LandingPage() {
   const tagsQuery = useTodoTagsAllQuery();
-  const tagsPost = useTodoTagsPOSTMutation();
   const itemsQuery = useTodoItemsAllQuery();
-  const itemsPost = useTodoItemsMutation();
 
   const [todoItems, setTodoItems] = useState<ITodoItemResponse[]>([]);
-
-  const addTag = (tag: ITodoTagResponse) => {
-    tagsPost.mutate(new TodoTagRequest({ ...tag }), {
-      onSuccess: () => tagsQuery.refetch(),
-    });
-  };
-
-  const addTodoItem = (item: ICreateTodoItemRequest) => {
-    itemsPost.mutate(new CreateTodoItemRequest({ ...item }), {
-      onSuccess: () => itemsQuery.refetch(),
-    });
-  };
 
   useEffect(() => {
     setTodoItems(!!itemsQuery.data ? itemsQuery.data : []);
@@ -43,7 +23,7 @@ export function LandingPage() {
 
   return (
     <>
-      <TodoTagEditor addTag={addTag} />
+      <TodoTagEditor />
       <TodoTagListView />
       <TodoItemEditor />
       <TodoItemListView todoItems={todoItems} />
