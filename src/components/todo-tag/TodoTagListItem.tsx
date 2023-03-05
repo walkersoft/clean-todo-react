0c6c7ba21ad1863d49d7cdda61072a541879649b
Jason L. Walker <jasonlanewalker@gmail.com>
2023-03-05 12:56:15 -0600

    Added tag unassignment functionality to tag item

--- src/components/todo-tag/TodoTagListItem.tsx
+++ src/components/todo-tag/TodoTagListItem.tsx
@@ -1,8 +1,12 @@
 import DeleteIcon from "@mui/icons-material/Delete";
+import PlaylistRemoveIcon from "@mui/icons-material/PlaylistRemove";
 import { IconButton, ListItem, ListItemText } from "@mui/material";
-import { ITodoTagResponse } from "../../api/api-client";
-import { useTodoTagsDELETEMutation } from "../../api/api-client/Query";
-import { useTagsDispatch } from "../../contexts/TagsContext";
+import { ITodoTagResponse, TodoTagRequest } from "../../api/api-client";
+import {
+  useTodoTagsDELETEMutation,
+  useUnassignMutation,
+} from "../../api/api-client/Query";
+import { refetchTagsDispatchAction, useTagsDispatch } from "../../contexts/TagsContext";
 
 interface TodoTagListItemProps {
   tag: ITodoTagResponse;
@@ -14,24 +18,42 @@ export function TodoTagListItem({ tag }: TodoTagListItemProps) {
   const dispatch = useTagsDispatch();
 
   const deleteTag = useTodoTagsDELETEMutation(tag.id, {
-    onSuccess: () => dispatch({ type: "require-refetch" }),
+    onSuccess: () => dispatch(refetchTagsDispatchAction),
+  });
+
+  const unassignTag = useUnassignMutation({
+    onSuccess: () => dispatch(refetchTagsDispatchAction),
   });
 
   const handleDelete = () => {
     deleteTag.mutate();
   };
 
+  const handleUnassign = () => {
+    unassignTag.mutate(new TodoTagRequest({ ...tag }));
+  };
+
   return (
     <ListItem
       secondaryAction={
-        <IconButton
-          edge="end"
-          color="error"
-          disabled={isAssigned}
-          onClick={handleDelete}
-        >
-          <DeleteIcon />
-        </IconButton>
+        <>
+          <IconButton
+            edge="end"
+            color="info"
+            disabled={!isAssigned}
+            onClick={handleUnassign}
+          >
+            <PlaylistRemoveIcon />
+          </IconButton>
+          <IconButton
+            edge="end"
+            color="error"
+            disabled={isAssigned}
+            onClick={handleDelete}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </>
       }
     >
       <ListItemText primary={name} />
