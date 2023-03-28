15e352bae152e68ffce54d1c45e58ffb39bb7ccf
Jason L. Walker <jasonlanewalker@gmail.com>
2023-03-27 19:32:02 -0500

    Removed editor from landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,10 +1,5 @@
-import { TodoItemEditor } from "../components/todo-item/TodoItemEditor";
 import { TodoItemTableView } from "../components/todo-item/TodoItemTableView";
 
 export function LandingPage() {
-  return (
-    <>
-      <TodoItemTableView />
-    </>
-  );
+  return <TodoItemTableView />;
 }
