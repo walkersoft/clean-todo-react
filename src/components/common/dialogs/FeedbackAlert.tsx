5ce6e99a7a39a376e5e7d8eeddf86f90a9a544a8
Jason L. Walker <jasonlanewalker@gmail.com>
2023-08-28 23:30:13 -0500

    Updated feedback component so that it's state is not controlled

--- src/components/common/dialogs/FeedbackAlert.tsx
+++ src/components/common/dialogs/FeedbackAlert.tsx
@@ -1,10 +1,10 @@
-import { AlertColor, Snackbar } from "@mui/material";
-import { useState } from "react";
+import { Alert, AlertColor, Snackbar } from "@mui/material";
 
 export interface FeedbackAlertProps {
   message: string;
   severity: AlertColor;
-  isOpen: boolean;
+  open: boolean;
+  handleClose: () => void;
   vertical?: "bottom" | "top";
   horizontal?: "left" | "right" | "center";
 }
@@ -12,27 +12,21 @@ export interface FeedbackAlertProps {
 export function FeedbackAlert({
   message,
   severity,
-  isOpen,
+  open,
+  handleClose,
   vertical = "bottom",
   horizontal = "center",
 }: FeedbackAlertProps) {
-  const [open, setOpen] = useState(isOpen);
-
-  const handleClose = (_: React.SyntheticEvent | Event, reason?: string) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setOpen(false);
-  };
-
   return (
     <Snackbar
       anchorOrigin={{ vertical, horizontal }}
       open={open}
       autoHideDuration={7000}
       onClose={handleClose}
-      message={message}
-    />
+    >
+      <Alert severity={severity} onClose={handleClose}>
+        {message}
+      </Alert>
+    </Snackbar>
   );
 }
